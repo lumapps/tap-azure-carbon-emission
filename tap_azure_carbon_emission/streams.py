66814b9cf3de2201a358d41c94258b86c35aafb9
Jonathan Perron <jonathan.perron@lumapps.com>
2025-11-19 16:39:00 +0100

    fix(tap): fix issues when requesting azure APIs
    Changelog
    * Fix missing parameter `token`  in call to BearerTokenAuthenticator
    * Remove parameter `date` which is not returned by the current version of the API

--- tap_azure_carbon_emission/streams.py
+++ tap_azure_carbon_emission/streams.py
@@ -9,8 +9,8 @@ SCHEMAS_DIR = resources.files(__package__) / "schemas"
 class CarbonEmissionReportStream(AzureCarbonStream):
     name = "carbon_emission_reports"
     path = "/providers/Microsoft.Carbon/carbonEmissionReports?api-version=2025-04-01"
-    primary_keys = ["dataType", "itemName"]  # Use actual fields that exist in records
-    replication_key = "date"  # Use date field that exists in the response
+    primary_keys = ["dataType", "itemName"]
+    replication_key = None
     rest_method = "POST"
     schema_filepath = SCHEMAS_DIR / "carbon_emission_report.json"
 
